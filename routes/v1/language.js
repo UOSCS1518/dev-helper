2fb548ecd3df4573a4d984ad507c7fc5a4477a50
김건회 <jkl9510357@gmail.com>
2019-09-22 18:18:27 +0900

    language helper 초안 및 테스트용 api

--- routes/v1/language.js
+++ routes/v1/language.js
@@ -6,7 +6,23 @@ router.post('/quesition', async (req, res) => {
     const lang = req.body.action.params.language
     const func = req.body.action.params.language_function
     try {
-        responseTemplate = languageDic[lang][func];
+        console.log(lang, func)
+        responseTemplate = languageDic[lang][func]();
+        responseBody = {
+            version: '2.0',
+            template: responseTemplate
+        }
+        res.status(200).send(responseBody)
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+router.get('/quesition/test', async (req, res) => {
+    const lang = req.query.language
+    const func = req.query.language_function
+    try {
+        responseTemplate = languageDic[lang][func]();
         responseBody = {
             version: '2.0',
             template: responseTemplate
